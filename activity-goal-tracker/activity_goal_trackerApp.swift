060d859c1df05759eda0075d360d070b2d988132
Tucker <borisblade17@gmail.com>
2023-06-02 22:24:38 -0500

    Initial Commit

---
+++ activity-goal-tracker/activity_goal_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  activity_goal_trackerApp.swift
+//  activity-goal-tracker
+//
+//  Created by tucker bichsel on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct activity_goal_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
